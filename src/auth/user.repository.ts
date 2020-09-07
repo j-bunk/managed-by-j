52df2db9d5cf1c6e1cf48096ae7b487b5640002c
Jihad Bunkheila <jihadzackaria@gmail.com>
2020-09-07 18:40:19 -0400

    feat: implemented user sign up with credential validation and duplicate username error handling

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,31 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      // Move error codes to enum file
+      // Refer to this https://www.h2database.com/javadoc/org/h2/api/ErrorCode.html
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
